da99d96a1a38156efd93820e001e676014f46815
Peter Kracik <peter@kracik.sk>
2021-03-10 07:35:11 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,11 +12,16 @@ mkdir "$SSH_PATH"
 echo "${INPUT_PRIVATEKEY}" > "$SSH_PATH/deploy_key"
 chmod 600 "$SSH_PATH/deploy_key"
 
+echo "# running scripts:" > ~/script.sh
 
 # Do deployment
 #sh -c "rsync $1 -e 'ssh -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no' $2 $GITHUB_WORKSPACE/ $3"
 for cmd in ${INPUT_COMMAND}; do
   echo -e "${BLUE}Syncinc: ${cmd}"
-  sh -c "${cmd}"
+  echo "${INPUT_COMMAND}" >> ~/script.sh
   echo ""
 done
+
+chmod +x ~/script.sh
+cat ~/script.sh
+~/script.sh
